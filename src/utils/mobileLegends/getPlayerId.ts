6f3a47e1529d8bfa97bcb0c1cbf684c6508fff07
Emillio Kanz <emilliokanz@gmail.com>
2025-05-12 11:51:42 +0700

    fixes: remove whitepace in required information when getting cust ID

--- src/utils/mobileLegends/getPlayerId.ts
+++ src/utils/mobileLegends/getPlayerId.ts
@@ -2,7 +2,9 @@ export function getMlPlayerId(data: any) {
     try {
         console.log(data, "data object")
         // const data = JSON.parse(jsonString);
-        return `${data.required_information.player_id}${data.required_information.zone_id}`;
+        return `${data.required_information.player_id.replace(/\s/g,'')
+        }${data.required_information.zone_id.replace(/\s/g,'')
+        }`;
     } catch (error) {
         console.error("Error parsing JSON:", error);
         return null;
@@ -13,7 +15,8 @@ export function getGarenaPlayerId(data: any) {
     try {
         console.log(data, "data object")
         // const data = JSON.parse(jsonString);
-        return `${data.required_information.player_id}`;
+        return `${data.required_information.player_id.replace(/\s/g,'')
+        }`;
     } catch (error) {
         console.error("Error parsing JSON:", error);
         return null;
