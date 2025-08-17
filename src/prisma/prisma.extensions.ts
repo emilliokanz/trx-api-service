7935e670926761adec652db277f034bcc47b685d
Emillio Kanz <25003075@AD.ALLOBANK.LOCAL>
2025-08-17 09:44:32 +0700

    get all bank account

--- src/prisma/prisma.extensions.ts
+++ src/prisma/prisma.extensions.ts
@@ -2,7 +2,7 @@ import { Prisma, Roles } from "@prisma/client";
 
 export function createUserTrackingExtension(getCurrentUser: () => { id: any, role: any }) {
     const excludeModelCreate = ['TransactionHistory', 'User']
-    const excludeModelFindMany = ['User', "ProductPrice", "ExternalUser", "ExternalProduct"]
+    const excludeModelFindMany = ['User', "ProductPrice", "ExternalUser", "ExternalProduct", "BankAccount"]
 
 
     return Prisma.defineExtension({
