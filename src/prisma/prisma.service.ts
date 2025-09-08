3c77b40c8c571c04e82661cbfff13634f24ddda5
Emillio Kanz <25003075@AD.ALLOBANK.LOCAL>
2025-09-08 14:04:40 +0700

    remove usage of extension for now

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,25 +1,25 @@
-import { Inject, Injectable, OnModuleInit, Scope } from '@nestjs/common';
+import { Inject, Injectable, OnModuleInit, Scope, OnModuleDestroy } from '@nestjs/common';
 import { REQUEST } from '@nestjs/core';
 import { PrismaClient } from '@prisma/client';
 import { createUserTrackingExtension } from './prisma.extensions';
 
-@Injectable({scope: Scope.REQUEST})
-export class PrismaService extends PrismaClient implements OnModuleInit {
-  constructor(@Inject(REQUEST) private readonly request: any){
+@Injectable()
+export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
+  constructor(){
     super();
   
-    const extendedClient = new PrismaClient().$extends(
-      createUserTrackingExtension(() => {
-        return {id: this.request.user?.id, role: this.request.user?.role}
-      })
-    )
-
-    Object.assign(this, extendedClient);
+    // this.$extends(
+    //   createUserTrackingExtension(() => {
+    //     return {id: this.request.user?.id, role: this.request.user?.role}
+    //   })
+    // )
   }
   
   async onModuleInit() {
     await this.$connect();
   }
 
-
+  async onModuleDestroy() {
+    await this.$disconnect(); // ✅ ensures connections are released
+  }
 }
