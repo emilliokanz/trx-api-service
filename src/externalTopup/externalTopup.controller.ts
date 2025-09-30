2f56c929169acb0f80c9f3b11e2d46ddad9b1083
Emillio Kanz <25003075@AD.ALLOBANK.LOCAL>
2025-09-30 22:01:06 +0700

    remove undefined validation

--- src/externalTopup/externalTopup.controller.ts
+++ src/externalTopup/externalTopup.controller.ts
@@ -45,7 +45,7 @@ export class ExternalTopupController {
   @Post('create-topup')
   @UsePipes(new ValidationPipe({ whitelist: true }))
   async createTopupRequest(@Body() payload: CreateTopupRequestDto, @Req() req: any) {
-    if (!req.headers['x-sign'] || req.headers['x-sign'] == undefined) {
+    if (!req.headers['x-sign']) {
       return new ApiResponseDto(errorMap[4011], null, '4011');
     }
 
