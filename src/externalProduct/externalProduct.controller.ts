b64659f29ca60f40cdac3a01fb050e8f0a5bbf76
Emillio Kanz <25003075@AD.ALLOBANK.LOCAL>
2025-10-26 15:08:32 +0700

    return get-product with response dto

--- src/externalProduct/externalProduct.controller.ts
+++ src/externalProduct/externalProduct.controller.ts
@@ -14,89 +14,94 @@ import { errorMap } from "src/lib/errorCodes";
 
 @Controller('/api/v1/product')
 export class ExternalProductController {
-    constructor(private extProductService: ExternalProductService, private extAuthService: ExternalAuthService) { }
+  constructor(private extProductService: ExternalProductService, private extAuthService: ExternalAuthService) { }
 
-    
-    @UseGuards(AuthGuard)
-    @UserRoles([Roles.SuperAdmin])@Post('/create')
-    async create(@Body() payload: CreateExtProduct[]) {
-        return await this.extProductService.createProduct(payload)
-    }
 
-    @UseGuards(AuthGuard)
-    @UserRoles([Roles.SuperAdmin])
-    @Post('/create-supplier')
-    async createSupplierProduct(@Body() payload: CreateSupplierProduct[]) {
-      const errors : any= [];
-    
-      for (let i = 0; i < payload.length; i++) {
-        const { errors: validationErrors } = await validateDto(CreateSupplierProduct, payload[i]);
-    
-        if (validationErrors.length > 0) {
-          errors.push({
-            index: i,
-            errors: validationErrors,
-          });
-        }
+  @UseGuards(AuthGuard)
+  @UserRoles([Roles.SuperAdmin]) @Post('/create')
+  async create(@Body() payload: CreateExtProduct[]) {
+    return await this.extProductService.createProduct(payload)
+  }
+
+  @UseGuards(AuthGuard)
+  @UserRoles([Roles.SuperAdmin])
+  @Post('/create-supplier')
+  async createSupplierProduct(@Body() payload: CreateSupplierProduct[]) {
+    const errors: any = [];
+
+    for (let i = 0; i < payload.length; i++) {
+      const { errors: validationErrors } = await validateDto(CreateSupplierProduct, payload[i]);
+
+      if (validationErrors.length > 0) {
+        errors.push({
+          index: i,
+          errors: validationErrors,
+        });
       }
-    
-      if (errors.length > 0) {
-        throw new HttpException(
-          new ApiResponseDto(errorMap[4001], errors, '4001'),
-          HttpStatus.BAD_REQUEST,
-        );
-      }
-    
-      return await this.extProductService.createSupplierProductFn(payload);
-    }
-    
-
-    @UseGuards(AuthGuard)
-    @UserRoles([Roles.SuperAdmin])
-    @Post('/update')
-    async update(@Body() payload: CreateExtProduct) {
-        return await this.extProductService.updateProduct(payload)
     }
 
-    @UseGuards(AuthGuard)
-    @UserRoles([Roles.Admin, Roles.SuperAdmin])
-    @Post('/get-list')
-    async getAll(@Body() payload: any, @Req() req: any) {
-        const user = await this.extAuthService.getUserDetail(req.headers.authorization)
-        return await this.extProductService.findProducts(payload.page, payload.size, payload.filter, user.role)
+    if (errors.length > 0) {
+      throw new HttpException(
+        new ApiResponseDto(errorMap[4001], errors, '4001'),
+        HttpStatus.BAD_REQUEST,
+      );
     }
 
-    @UseGuards(AuthGuard)
-    @UserRoles([Roles.Admin])
-    @Post('/create-customer')
-    async createCustomer(@Body() payload: CreateExtProductCustomer[], @Req() req: any) {
-        const user = await this.extAuthService.getUserDetail(req.headers.authorization)
+    return await this.extProductService.createSupplierProductFn(payload);
+  }
 
-        return await this.extProductService.createAdminCustProduct(payload, user.id)
-    }
 
-    @UseGuards(AuthGuard)
-    @UserRoles([Roles.Admin])
-    @Post('/update-customer')
-    async updateCustomer(@Body() payload: CreateExtProductCustomer, @Req() req: any) {
-        const user = await this.extAuthService.getUserDetail(req.headers.authorization)
+  @UseGuards(AuthGuard)
+  @UserRoles([Roles.SuperAdmin])
+  @Post('/update')
+  async update(@Body() payload: CreateExtProduct) {
+    return await this.extProductService.updateProduct(payload)
+  }
 
-        return await this.extProductService.updateAdminCustProduct(payload, user.id)
-    }
+  @UseGuards(AuthGuard)
+  @UserRoles([Roles.Admin, Roles.SuperAdmin])
+  @Post('/get-list')
+  async getAll(@Body() payload: any, @Req() req: any) {
+    const user = await this.extAuthService.getUserDetail(req.headers.authorization)
+    return await this.extProductService.findProducts(payload.page, payload.size, payload.filter, user.role)
+  }
 
-    @UseGuards(AuthGuard)
-    @UserRoles([Roles.Admin])
-    @Post('/delete-customer')
-    async deleteCustomer(@Body() payload: CreateExtProductCustomer, @Req() req: any) {
-        const user = await this.extAuthService.getUserDetail(req.headers.authorization)
+  @UseGuards(AuthGuard)
+  @UserRoles([Roles.Admin])
+  @Post('/create-customer')
+  async createCustomer(@Body() payload: CreateExtProductCustomer[], @Req() req: any) {
+    const user = await this.extAuthService.getUserDetail(req.headers.authorization)
 
-        return await this.extProductService.updateAdminCustProduct(payload, user.id)
-    }
+    return await this.extProductService.createAdminCustProduct(payload, user.id)
+  }
 
-    @UseGuards(AuthGuard)
-    @UserRoles([Roles.SuperAdmin])
-    @Post('/get-product')
-    async updateDigi() {
-        return await this.extProductService.getDigiflazzPrice()
-    }
+  @UseGuards(AuthGuard)
+  @UserRoles([Roles.Admin])
+  @Post('/update-customer')
+  async updateCustomer(@Body() payload: CreateExtProductCustomer, @Req() req: any) {
+    const user = await this.extAuthService.getUserDetail(req.headers.authorization)
+
+    return await this.extProductService.updateAdminCustProduct(payload, user.id)
+  }
+
+  @UseGuards(AuthGuard)
+  @UserRoles([Roles.Admin])
+  @Post('/delete-customer')
+  async deleteCustomer(@Body() payload: CreateExtProductCustomer, @Req() req: any) {
+    const user = await this.extAuthService.getUserDetail(req.headers.authorization)
+
+    return await this.extProductService.updateAdminCustProduct(payload, user.id)
+  }
+
+  @UseGuards(AuthGuard)
+  @UserRoles([Roles.SuperAdmin])
+  @Post('/get-product')
+  async updateDigi() {
+
+    const product = await this.extProductService.getDigiflazzPrice()
+    return new ApiResponseDto("success", {
+      product
+    }, '0000')
+
+  }
 }
